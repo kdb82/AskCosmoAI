906461eb01e6dcca75ff5f8d144aceb20f186a98
AndersonBillss <anderson@thebillsfamily.com>
2025-10-13 13:38:45 -0600

    Added hey-api client to auto-generate api calls

--- web/api/Program.cs
+++ web/api/Program.cs
@@ -4,12 +4,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("dev", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("dev");
 }
 else
 {
